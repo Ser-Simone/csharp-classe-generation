939b2f7008a959934ed8ac3d07e851032791076d
Simone Serpi <simoneserpi@hotmail.it>
2022-04-06 19:52:03 +0200

    Fine_Bonus

--- ClasseGeneration/Program.cs
+++ ClasseGeneration/Program.cs
@@ -1,29 +1,14 @@
-﻿/*
-A questo punto volete avere delle statistiche sulla vostra classe, pertanto vi si chiede di fornire tramite 
-funzioni le seguenti cose:
-una funzione calcolaEtaMediaClasse che vi restituisca l'età media della classe.
-una funzione EtaPiuGiovane che vi restituisce l'età dell'alunno più giovane in classe.
-una funzione EtàPiuVecchio che vi restituisce l'età dell'alunno più vecchio.
-Testate le vostre funzioni di statistiche, per esempio aggiungendo l'opzione "statistiche" per l'utente,
-che una volta scritto a console "statistiche" vi stampi i risultati delle funzioni appena dichiarate.
- */
-
-
-
-
-int postiMax = 2;
+﻿int postiMax = 2;
 int numeroPartecipanti = 0;
 string[] arrayCognomi = new string[postiMax];
 int[] arrayEta = new int[postiMax];
 string[] arrayNomi = new string[postiMax];
-bool input;
+bool input = true;
 // -------------------- MAIN ------------------------
 
 Console.WriteLine("Il numero di posti disponibili in classe è " + postiMax);
-Console.WriteLine("Nella tua classe sono presenti i seguenti alunni:");
 
-
-while (true)
+while (input = true)
 {
     Console.Write("Vuoi aggiungere o rimuovere un Alunno [aggiungi/rimuovi]? ");
     string risposta = Console.ReadLine();
@@ -33,12 +18,10 @@ while (true)
         case "aggiungi":
             Console.WriteLine("Scrivi il nome dell'alunno: ");
             string nomiAlunni = Console.ReadLine();
-            input = true;   
-
+            input = true;
             Console.WriteLine("Scrivi il cognome dell'alunno: ");
              string cognomiAlunno = Console.ReadLine();
             
-
             Console.WriteLine("Scrivi l'età dell'alunno: ");
             int etaAlunno = int.Parse(Console.ReadLine());
            
@@ -49,22 +32,35 @@ while (true)
             input = true;
             break;
         case "no":
+            Console.WriteLine("scelta non valida");
             input = false;
             break;
-        
-    }
-    stampaArray(arrayNomi);
-    stampaArray(arrayCognomi);
-    stampaArrayEta(arrayEta);
-    int media =calcolaEtaMediaClass(arrayEta);
-    Console.WriteLine("la media della classe è:" +media);
+        case "statistiche":
 
-    int alunnoGiovane = EtaPiuGiovane(arrayEta);
-    Console.WriteLine("il più piccolo è:" +alunnoGiovane);
+            stampaArray(arrayNomi);
+            Console.WriteLine("\n");
+            stampaArray(arrayCognomi);
+            Console.WriteLine("\n");
+            stampaArrayEta(arrayEta);
+            Console.WriteLine("\n");
+            calcolaEtaMediaClass(arrayEta);
+            Console.WriteLine("\n");
+
+          
+           int indice = alunnoPiuGrande(arrayEta);
+            Console.WriteLine( arrayNomi[indice] + arrayCognomi[indice] + "\n");
+
+            int indicepiccolo = EtaPiuGiovane(arrayEta);
+            Console.WriteLine(arrayNomi[indicepiccolo] + arrayCognomi[indicepiccolo] + "\n");
+            break;
+    }
+    
 }
 
-    //---------------- FUNZIONI -------------------------
-    void stampaArray(string[] array)
+
+
+//---------------- FUNZIONI -------------------------
+void stampaArray(string[] array)
     {
         Console.Write("[");
         for (int i = 0; i < array.Length; i++)
@@ -121,29 +117,51 @@ while (true)
         }
     }
 
-int calcolaEtaMediaClass (int[] array)
+void calcolaEtaMediaClass (int[] array)
 {
     int somma = 0;
-    int media = 0;
+    double media = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        
+        if (array[i] != 0)
+        {
             somma += array[i];
+        }
+            
         
     }
     media = somma/arrayEta.Length;
-    return media;
+    Console.WriteLine("la media è" +media);
 
 }
 
-int EtaPiuGiovane (int[] array)
+int alunnoPiuGrande(int[] array)
 {
-    int minore = 0;
+    int j = 0;
+    int etàMaggiore = 0;
     for (int i = 0; i < array.Length; i++)
     {
-
-        minore = Math.Min(minore, array[i]);
-
+        if (etàMaggiore < array[i])
+        {
+            etàMaggiore = array[i];
+            j = i;
+        }
     }
-    return minore;
+    Console.Write("L'alunno piu grande della classe ha: " + etàMaggiore +"anni");
+    return j;
+}
+int EtaPiuGiovane(int[] array)
+{
+    int j = 0;
+    int etaMinore = 1000;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( array[i] < etaMinore)
+        {
+            etaMinore = array[i];
+            j = i;
+        }
+    }
+    Console.Write("L'alunno piu piccolo della classe ha: \n" + etaMinore + "anni \n");
+    return j;
 }
